f57970f5003d5c37335e03c06b5f9fb38b69d1c7
syusi <cs15044@s.inf.shizuoka.ac.jp>
2020-01-02 00:23:35 +0900

    クロスオリジン回避サーバーの追加

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(req, res) {
+    data = fs.readFileSync('./main.html');
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+}).listen(8080);
+console.log("server start!!");
