8a1f57fc05479ef92c3fd58fad763d902ac5a826
syusi <cs15044@s.inf.shizuoka.ac.jp>
2020-01-06 01:13:21 +0900

    アニメーションまで試した

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function(req, res) {
-    data = fs.readFileSync('./main.html');
+    data = fs.readFileSync('./docs/main.html');
     res.writeHead(200,{'Content-Type': 'text/html'});
     res.write(data);
     res.end();
